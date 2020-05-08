7384ccbc08537a7fbd09874e2bcdbb5d59ccea09
Alvin Ghouas <aghouas@adobe.com>
2020-05-07 17:48:35 -0700

    vgrid example using json

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import {VGrid, Table, Database, IntervalSet, Interval, Bounds, BoundingBox, SpatialType_Bbox, interval_blocks_from_json} from '@wcrichto/vgrid';
+import '@wcrichto/vgrid/dist/vgrid.css';
+import vgrid_spec from './vgrid_spec.json';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let {interval_blocks, database, settings} = vgrid_spec;
+  database = Database.from_json(database);
+  interval_blocks = interval_blocks_from_json(interval_blocks);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  // Run code when user provides labeling input
+  let label_callback = (label_state) => {
+    console.log(label_state.blocks_selected);
+  };
+  // Render React component into a <div id="#vgrid"></div>
+  ReactDOM.render(
+    <VGrid interval_blocks={interval_blocks} database={database}
+           settings={settings} label_callback={label_callback} />,
+    document.getElementById('vgrid'));
+
